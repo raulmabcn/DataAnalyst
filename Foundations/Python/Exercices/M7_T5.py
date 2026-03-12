6877296884348d7fcc44bcb59439c550792fae0e
raulmabcn <raulmabcn@gmail.com>
2026-03-12 09:54:58 +0100

    remove single exercices

--- Foundations/Python/Exercices/M7_T5.py
+++
@@ -1,9 +0,0 @@
-""" Crea una funció que rebi un text i retorni el text en majúscules. 
-Demana un text a l’usuari i mostra el resultat amb la funció.  """
-
-
-def upper_case( text ):
-    return text.upper()
-
-user_text = input("Escriu un text: " )
-print( upper_case( user_text ) )
