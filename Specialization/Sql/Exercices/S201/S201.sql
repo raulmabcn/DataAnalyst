f4339734b996eb16da8d17876468eb9e94ad2994
raulmabcn <raulmabcn@gmail.com>
2026-03-19 16:50:46 +0100

    S201 Exercise firsts steps

---
+++ Specialization/Sql/Exercices/S201/S201.sql
@@ -0,0 +1,95 @@
+-- Nivell 1 ------------------------------------------------------------------------------------------------------
+-- Exercici 2 --------------------------------------------------------
+-- Utilitzant JOIN realitzaràs les següents consultes ----------------
+
+-- Llistat dels països que estan fent compres.
+
+SELECT DISTINCT c.country
+FROM company AS c 
+INNER JOIN transaction AS t ON t.company_id = c.id;
+
+-- Des de quants països es realitzen les compres.
+
+SELECT COUNT(DISTINCT c.country)
+FROM company AS c 
+INNER JOIN transaction AS t ON t.company_id = c.id;
+
+-- Identifica la companyia amb la mitjana més gran de vendes.
+
+SELECT c.id, c.company_name, AVG(t.amount) AS avg_sales
+FROM company AS c
+INNER JOIN transaction AS t ON t.company_id = c.id
+GROUP BY c.id, c.company_name
+ORDER BY avg(t.amount) DESC
+LIMIT 1;
+
+-- Exercici 3 --------------------------------------------------------
+-- (Utilitzant només subconsultes (sense utilitzar JOIN)--------------
+
+-- Mostra totes les transaccions realitzades per empreses d'Alemanya.
+
+SELECT *
+FROM transaction
+WHERE company_id IN ( 
+	SELECT id
+    FROM company
+    WHERE country = 'Germany'
+);
+
+-- Llista les empreses que han realitzat transaccions per un amount superior a la mitjana de totes les transaccions.
+
+SELECT company_name
+FROM company 
+WHERE id IN ( 
+	SELECT company_id
+	FROM transaction
+    WHERE amount > ( SELECT AVG(amount) FROM transaction ) 
+);
+
+-- Eliminaran del sistema les empreses que no tenen transaccions registrades, entrega el llistat d'aquestes empreses.
+
+SELECT c.id
+FROM company c
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM transaction t
+    WHERE t.company_id = c.id
+);
+
+-- Nivell 2 ------------------------------------------------------------------------------------------------------
+
+-- Exercici 1 --------------------------------------------------------
+-- Identifica els cinc dies que es va generar la quantitat més gran d'ingressos a l'empresa per vendes. 
+-- Mostra la data de cada transacció juntament amb el total de les vendes.
+
+
+
+
+-- Exercici 2 --------------------------------------------------------
+-- Quina és la mitjana de vendes per país? Presenta els resultats ordenats de major a menor mitjà.
+
+
+-- Exercici 3 --------------------------------------------------------
+-- En la teva empresa, es planteja un nou projecte per a llançar algunes campanyes publicitàries per a fer competència a la companyia "Non Institute". 
+-- Per a això, et demanen la llista de totes les transaccions realitzades per empreses que estan situades en el mateix país que aquesta companyia.
+-- (Mostra el llistat aplicant JOIN i subconsultes.)
+-- (Mostra el llistat aplicant solament subconsultes.)
+
+
+-- Nivell 3 ------------------------------------------------------------------------------------------------------
+
+-- Exercici 1 --------------------------------------------------------
+-- Presenta el nom, telèfon, país, data i amount, d'aquelles empreses que van realitzar transaccions amb un valor comprès entre 100 i 200 euros 
+-- i en alguna d'aquestes dates: 29 d'abril del 2021, 20 de juliol del 2021 i 13 de març del 2022. Ordena els resultats de major a menor quantitat.
+
+
+-- Exercici 2 --------------------------------------------------------
+-- Necessitem optimitzar l'assignació dels recursos i dependrà de la capacitat operativa que es requereixi, 
+-- per la qual cosa et demanen la informació sobre la quantitat de transaccions que realitzen les empreses, 
+-- però el departament de recursos humans és exigent i vol un llistat de les empreses on especifiquis si tenen més de 4 transaccions o menys.
+
+
+
+
+
+
